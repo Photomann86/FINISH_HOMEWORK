240359eb72fd44adc2b29a669d0f2f9d1e68226f
Photomann86 <kunzler555@gmail.com>
2023-01-26 22:40:18 +0500

    создана программа по условию задачи

---
+++ task_of_homework/task_of_homework.cs
@@ -0,0 +1,33 @@
+﻿/* Написать программу, которая из имеющегося 
+массива строк формирует массив из строк, длинна которых 
+не более трёх символов. Первоначальный массив можно ввести с клавиатуры 
+или задать на старте выполнения алгоритма. 
+При реешении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами */
+string[] firstMassiv = new string[5] {"7890", "hello", "01", "Hi", "765"}; //создание массива строк
+string[] secondMassiv = new string[firstMassiv.Length];
+//метод выбора строк с количеством символов не более 3
+void SecondArray(string[] firstMassiv, string[] secondMasiv)
+{
+    int count = 0;
+    for (int i=0; i<firstMassiv.Length; i++)
+    {
+        if (firstMassiv[i].Length<=3)
+        {
+        secondMassiv[count] = firstMassiv[i];
+        count++;
+        }
+    }
+}
+//метод вывода массива строк на экран
+void PrintMassivOfStrings(string[] Massiv)
+{
+    for (int i=0; i < Massiv.Length; i++)
+    {
+    Console.Write($"{Massiv[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(firstMassiv, secondMassiv);
+PrintMassivOfStrings(firstMassiv); //вывод исходного массива
+PrintMassivOfStrings(secondMassiv); //вывод результата
