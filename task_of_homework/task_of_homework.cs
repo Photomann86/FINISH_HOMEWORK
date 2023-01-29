cc4392a8e8468312e2593d0bf2c6e466b99c73d1
Photomann86 <kunzler555@gmail.com>
2023-01-29 23:08:30 +0500

    программа работает согласно заданию

--- task_of_homework/task_of_homework.cs
+++ task_of_homework/task_of_homework.cs
@@ -2,32 +2,40 @@
 массива строк формирует массив из строк, длинна которых 
 не более трёх символов. Первоначальный массив можно ввести с клавиатуры 
 или задать на старте выполнения алгоритма. 
-При реешении не рекомендуется пользоваться коллекциями, 
+При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами */
-string[] firstMassiv = new string[5] {"7890", "hello", "01", "Hi", "765"}; //создание массива строк
-string[] secondMassiv = new string[firstMassiv.Length];
-//метод выбора строк с количеством символов не более 3
-void SecondArray(string[] firstMassiv, string[] secondMassiv)
+int CountOfStrings(string message)
 {
-    int count = 0;
-    for (int i=0; i<firstMassiv.Length; i++)
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int size = CountOfStrings("введите количество строковых элементов: ");
+string[] ArrayOfStrings = new string[size];
+for (int i=0; i<size; i++)
+{
+    Console.WriteLine($"введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    ArrayOfStrings[i] = element;
+}
+string[] FinalArray = new string[size];
+int length = 3;
+int pos = 0;
+for (int j=0; j<size; j++)
+{
+    if (ArrayOfStrings[j].Length<=length)
     {
-        if (firstMassiv[i].Length<=3)
-        {
-        secondMassiv[count] = firstMassiv[i];
-        count++;
-        }
+        FinalArray[pos] = ArrayOfStrings[j];
+        pos++;
     }
 }
-//метод вывода массива строк на экран
-void PrintMassivOfStrings(string[] Massiv)
+void PrintFinal(string[] array)
 {
-    for (int i=0; i < Massiv.Length; i++)
+    for (int i=0; i<array.Length; i++)
     {
-    Console.Write($"{Massiv[i]} ");
+        Console.Write(array[i] + " ");
+    
     }
     Console.WriteLine();
 }
-SecondArray(firstMassiv, secondMassiv);
-PrintMassivOfStrings(firstMassiv); //вывод исходного массива
-PrintMassivOfStrings(secondMassiv); //вывод результата
+Console.WriteLine("массив элементов из трёх и менее символов");
+PrintFinal(FinalArray);
